d748f944856c03aaebc09736af30233fb7f454af
동하 <mcpelee@gmail.com>
2020-12-02 12:18:49 +0900

    Update kakao.php

--- kakao.php
+++ kakao.php
@@ -1,6 +1,6 @@
 <?php
  $session_start();
- $client_id = "4408b5bb51bdf4c89879e933556a21e8"; //★ 수정 할 것
+ $client_id = "444444444444444444444444444444444"; //★ 수정 할 것
  $redirect_uri = urlencode("http://localhost/callBackForKakao.php"); //★ 수정 할 것
  $kakaoLoginUrl = "https://kauth.kakao.com/oauth/authorize?client_id=".$client_id."&redirect_uri=".$redirect_uri."&response_type=code";
 ?>
@@ -11,7 +11,7 @@
   <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
     <script>
         // SDK를 초기화 합니다. 사용할 앱의 JavaScript 키를 설정해 주세요.
-        Kakao.init('2d68640b56d986af5c8a48505c7c8c71'); //★ 수정 할 것
+        Kakao.init('2222222222222222222222222222222222'); //★ 수정 할 것
 
         // SDK 초기화 여부를 판단합니다.
         console.log(Kakao.isInitialized());
